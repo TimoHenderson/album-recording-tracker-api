c22da15e5ae23171268d9c1baf2e57fb4733eec4
Timo Henderson <46020614+TimoHenderson@users.noreply.github.com>
2023-06-08 13:24:44 +0100

    removed client

--- recording-tracker-react/src/utils/updateStateData.js
+++
@@ -1,9 +0,0 @@
-export const updateStateData = (stateData, element) => {
-    let parentIds = [];
-    let currElement = element;
-    while (currElement.parentId) {
-        parentIds.push(currElement.parentId);
-    }
-
-
-}
